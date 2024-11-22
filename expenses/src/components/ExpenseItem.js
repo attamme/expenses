ed6c2f576f296a9d90a723cec9abe6f74f77b494
Annaliisa Tamme <annaliisa.tamme@voco.ee>
2024-11-22 09:02:46 +0200

    created date into different components

--- expenses/src/components/ExpenseItem.js
+++ expenses/src/components/ExpenseItem.js
@@ -1,13 +1,15 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
 
-    console.log(props);
-    console.log(props.data);
+    const day = props.data.date.toLocaleString('en-US', {day: '2-digit'});
+    const month = props.data.date.toLocaleString('en-US', {month: 'long'});
+    const year = props.data.date.getFullYear();
 
     return (
         <div className='expense-item'>
-            <div>{props.data.date.toString()}</div>
+            <ExpenseDate date={props.data.date}/>
             <div className='expense-item__description'>
                 <h2>{props.data.title}</h2>
                 <div className='expense-item__price'>{props.data.price}</div>
