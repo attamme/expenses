e667be6e5bda3a4ac61bca2b927f4f0d9c887c77
Annaliisa Tamme <annaliisa.tamme@voco.ee>
2024-11-22 10:23:57 +0200

    ulesanne 3

--- expenses/src/components/ExpenseItem.js
+++ expenses/src/components/ExpenseItem.js
@@ -1,17 +1,18 @@
 import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
     const{date, title, price} = props;
 
     return (
-        <div className='expense-item'>
+        <Card className='expense-item'>
             <ExpenseDate date={date}/>
             <div className='expense-item__description'>
                 <h2>{title}</h2>
                 <div className='expense-item__price'>{price}</div>
             </div>
-        </div>
+        </Card>
 );
 };
 
