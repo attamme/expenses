18ff4a53abaae079f9ad56fb90401572fc2ec369
Annaliisa Tamme <annaliisa.tamme@voco.ee>
2025-03-21 13:46:15 +0200

    valikuline - js koodi abil

--- expenses/src/components/Expenses/Expenses.js
+++ expenses/src/components/Expenses/Expenses.js
@@ -17,18 +17,18 @@ const Expenses = (props) => {
         return expense.date.getFullYear().toString() === filteredYear;
     });
 
+    let expensesContent = <p>No expenses found.</p>;
+
+    if(filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map((expense) => {
+            return <ExpenseItem date={expense.date} key={expense.id}/> 
+        })
+    }
+
     return (
             <Card className="expenses">
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-                {
-                  filteredExpenses.length === 0 && <p>No expenses found.</p>
-                }
-                {
-                  filteredExpenses.length > 0 && filteredExpenses.map((expense) => {
-                    return <ExpenseItem date={expense.date} key={expense.id}/> 
-                  })
-                }
-              
+                {expensesContent}
             </Card>
   );
 };
