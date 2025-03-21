0c314312b554882e7ffe50f4830f0379ffcf50c1
Annaliisa Tamme <annaliisa.tamme@voco.ee>
2025-03-21 14:06:00 +0200

    valikuline - eraldi komponendi abil

--- expenses/src/components/Expenses/Expenses.js
+++ expenses/src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, {useState} from 'react';
 import './Expenses.css';
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
+import ExpenseList from './ExpensesList';
 
 const Expenses = (props) => {
   const {expenses} = props;
@@ -17,18 +17,11 @@ const Expenses = (props) => {
         return expense.date.getFullYear().toString() === filteredYear;
     });
 
-    let expensesContent = <p>No expenses found.</p>;
-
-    if(filteredExpenses.length > 0) {
-        expensesContent = filteredExpenses.map((expense) => {
-            return <ExpenseItem date={expense.date} key={expense.id}/> 
-        })
-    }
 
     return (
             <Card className="expenses">
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-                {expensesContent}
+                <ExpenseList expenses={filteredExpenses}/>
             </Card>
   );
 };
